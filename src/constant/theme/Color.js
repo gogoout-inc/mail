5b5e0ab8af9bb09a0fb9903754f679c44fdb9236
mars liu <81066541+mars199704@users.noreply.github.com>
2022-12-06 18:07:05 +0800

    tmp

---
+++ src/constant/theme/Color.js
@@ -0,0 +1,42 @@
+const Color = {
+  white: '#ffffff',
+  black: '#121212',
+  primary: '#08415c',
+  secondary: {
+    DEFAULT: '#4199d5',
+    dark: '#3584ba',
+    select: '#b4e1ff',
+    duration: '#f0f9ff'
+  },
+  grey: {
+    11: '#1F1F1F',
+    10: '#262626',
+    9: '#434343',
+    8: '#595959',
+    7: '#8C8C8C',
+    6: '#BFBFBF',
+    5: '#D9D9D9',
+    4: '#F0F0F0',
+    3: '#F5F5F5',
+    2: '#FAFAFA',
+    1: '#FFFFFF'
+  },
+  accent: {
+    default: '#276EF1',
+    light: '#EFF3FE'
+  },
+  negative: {
+    default: '#E11900',
+    light: '#FFEFED'
+  },
+  warning: {
+    default: '#FFC043',
+    light: '#FFFAF0'
+  },
+  positive: {
+    default: '#05944F',
+    light: '#E6F2Ed'
+  }
+}
+
+export default Color
---
+++ src/constant/theme/Font.js
@@ -0,0 +1,18 @@
+const fontFamily = 'sans-serif'
+
+const Font = {
+  title: {
+    'font-size': '20px',
+    'font-weight': '700',
+    'line-height': '28px',
+    'font-family': fontFamily
+  },
+  subtitle: {
+    'font-size': '14px',
+    'font-weight': '700',
+    'line-height': '24px',
+    'font-family': fontFamily
+  }
+}
+
+export default Font
