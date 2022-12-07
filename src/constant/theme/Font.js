98defe13c709bb6cb8f9e6e1e0d9542becbcf0fa
mars liu <81066541+mars199704@users.noreply.github.com>
2022-12-07 12:24:05 +0800

    initial project, add orderEmail

--- src/constant/theme/Font.js
+++ src/constant/theme/Font.js
@@ -2,15 +2,18 @@ const fontFamily = 'sans-serif'
 
 const Font = {
   title: {
-    'font-size': '20px',
+    'font-size': '24px',
     'font-weight': '700',
-    'line-height': '28px',
+    'line-height': '36px',
     'font-family': fontFamily
   },
   subtitle: {
     'font-size': '14px',
     'font-weight': '700',
-    'line-height': '24px',
+    'line-height': '21px',
+    'font-family': fontFamily
+  },
+  content: {
     'font-family': fontFamily
   }
 }
