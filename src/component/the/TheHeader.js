71b936edd9c0b13a7d73cb48dcb77d454c2151a1
mars liu <81066541+mars199704@users.noreply.github.com>
2022-12-09 10:39:51 +0800

    add welcome template, footer, header

---
+++ src/component/the/TheHeader.js
@@ -0,0 +1,15 @@
+const header = {
+  tagName: 'mj-head',
+  children: [
+    {
+      tagName: 'mj-title',
+      content: 'gogoout租車通'
+    },
+    {
+      tagName: 'mj-breakpoint',
+      width: '320px'
+    }
+  ]
+}
+
+export default header
